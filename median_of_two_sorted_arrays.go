7115f51cd8e3563a73aa8c4d1275ff6f17321354
Norman Chen <s965202030@gmail.com>
2018-10-03 00:41:39 +0800

    fix bugs

--- median_of_two_sorted_arrays.go
+++ median_of_two_sorted_arrays.go
@@ -7,7 +7,7 @@ import (
 func findMedianSortedArrays(nums1 []int, nums2 []int) (result float64) {
 	nums := append(nums1, nums2...)
 	sort.Ints(nums)
-	numsLeng := len(nums) % 2
+	numsLeng := len(nums)
 	mid := numsLeng / 2
 	if numsLeng%2 == 0 {
 		result = float64(nums[mid-1]+nums[mid]) / 2
