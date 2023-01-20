939d1d7861f06110f11403a93bca91fa1b70cca3
n0rmanc <s965202030@gmail.com>
2023-01-20 17:54:57 +0800

    implements remove duplicates from sorted array

--- RemoveDuplicatesfromSortedArray.ts
+++ RemoveDuplicatesfromSortedArray.ts
@@ -1,3 +1,9 @@
 export function removeDuplicates(nums: number[]): number {
-    return 0;
-};
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
+}; 
